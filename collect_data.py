b3f7dd3219c9cfe9d3760a62fa5f6d466f39505c
Pratima <pratsnagare08@gmail.com>
2020-08-01 21:31:56 +0530

    Files for data generation

---
+++ collect_data.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+import os
+import sys
+
+
+label_map = {
+    "0": "Rock",
+    "1": "Paper",
+    "2": "Scissors"
+} 
+print("Please select one of the labels:")
+print("Rock(0)")
+print("Paper(1)")
+print("Scissors(2)")
+label = label_map.get(input(f"Your input: "))
+if not label:
+    print("Label not specified")
+print("\n"+label,"selected!")
+
+
+PATH = os.getcwd()+'\\'
+cap = cv2.VideoCapture(0)
+SAVE_PATH = os.path.join(PATH, label)
+
+if not os.path.exists(SAVE_PATH):
+    os.mkdir(SAVE_PATH)
+    
+num_imgs = int(input(f"No. of images do you wish to capture:"))
+print("Hit Space to Capture Image")
+
+count = 0
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Collecting Data : '+label,frame)
+    if cv2.waitKey(1) & 0xFF == ord(' '):
+        cv2.imwrite(SAVE_PATH+'\\'+label+'_{}.jpg'.format(count),frame)
+        print(SAVE_PATH+'\\'+label+'_{}.jpg Captured'.format(count))
+        count+=1
+    if count >= num_imgs:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
